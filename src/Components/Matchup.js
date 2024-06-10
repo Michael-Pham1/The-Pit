29f1b9d938582f8fc572cbb61adaf01d1655e3e9
ThomasMerritt <84432380+ThomasMerritt@users.noreply.github.com>
2024-06-10 01:41:03 -0700

    stuff

--- src/Components/Matchup.js
+++ src/Components/Matchup.js
@@ -1,9 +1,48 @@
+// import React from 'react';
+// import "./Matchup.css";
+// import vs from '../Images/vs-image.jpg';
+
+// // Dynamic props for displaying matchup information
+// function Matchup({ anime1Image, anime2Image, anime1Name, anime2Name, result }) {
+//   return (
+//     <>
+//       <div className='matchup-container'>
+//         <div className="matchup">
+//           <div className="character-1">
+//             <img src={anime1Image} alt={anime1Name} />
+//           </div>
+//           <div className="character-2">
+//             <img src={anime2Image} alt={anime2Name} />
+//             <div className="character-name">{anime2Name}</div>
+//           </div>
+//           <div className="vs">
+//             <img src={vs} alt="VS" />
+//           </div>
+          
+//         </div>
+//         <div className="vs-text">
+//           <div className="text-1">{anime1Name}</div>
+//           <div className="text-2">{anime2Name}</div>
+//         {/* <div className="vs-text-3">{result}</div> */}
+//         </div>
+//       </div>
+    
+//     </>
+    
+//   );
+// }
+
+// export default Matchup;
+
 import React from 'react';
 import "./Matchup.css";
 import vs from '../Images/vs-image.jpg';
 
-// Dynamic props for displaying matchup information
-function Matchup({ anime1Image, anime2Image, anime1Name, anime2Name, result }) {
+// Dynamic props for displaying matchup information, now including createDate
+function Matchup({ anime1Image, anime2Image, anime1Name, anime2Name, createDate }) {
+  // Format date if createDate is a Date object, otherwise leave as is
+  const formattedDate = createDate instanceof Date ? createDate.toLocaleDateString() : createDate;
+
   return (
     <>
       <div className='matchup-container'>
@@ -18,17 +57,15 @@ function Matchup({ anime1Image, anime2Image, anime1Name, anime2Name, result }) {
           <div className="vs">
             <img src={vs} alt="VS" />
           </div>
-          
         </div>
         <div className="vs-text">
           <div className="text-1">{anime1Name}</div>
           <div className="text-2">{anime2Name}</div>
-        {/* <div className="vs-text-3">{result}</div> */}
         </div>
+        {/* Display createDate */}
+        <div className="create-date">Created on: {formattedDate}</div>
       </div>
-    
     </>
-    
   );
 }
 
